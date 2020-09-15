26b60a536e941efb44e0713329bb54997b1c3d2c
muzi.he <muzi.habo@gmail.com>
2020-09-15 21:39:06 +0800

    bytedance：最大岛屿面积

---
+++ src/leetcode/editor/cn/bytedance/Solution009.java
@@ -0,0 +1,69 @@
+package leetcode.editor.cn.bytedance;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+ *
+ * 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。
+ * 你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ *
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ *  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ *  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ *  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。
+ *
+ * 示例 2:
+ *
+ * [[0,0,0,0,0,0,0,0]]
+ * 对于上面这个给定的矩阵, 返回 0。
+ *
+ *
+ *
+ * 注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+ *
+ * @author ziwen
+ * @since 2020-09-15 18:30
+ */
+public class Solution009 {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                int[][] currenGrid = grid.clone();
+                if (grid[i][j] == 1) {
+                    max = Math.max(max, dfs(currenGrid, i, j));
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 图的深度优先遍历
+     */
+    public int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        int num = 1;
+        grid[i][j] = 0;
+        num += dfs(grid, i, j - 1);
+        num += dfs(grid, i, j + 1);
+        num += dfs(grid, i - 1, j);
+        num += dfs(grid, i + 1, j);
+        return num;
+    }
+}
