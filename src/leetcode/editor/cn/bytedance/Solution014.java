64b6ae14d5542f6728ebaf1e2ce77de1ce61d19e
muzi.he <muzi.habo@gmail.com>
2020-09-17 20:17:58 +0800

    bytedance：第k个排列

---
+++ src/leetcode/editor/cn/bytedance/Solution014.java
@@ -0,0 +1,67 @@
+package leetcode.editor.cn.bytedance;
+
+import java.util.*;
+
+/**
+ * 给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。
+ *
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+ *
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 给定 n 和 k，返回第 k 个排列。
+ *
+ * 说明：
+ *
+ * 给定 n 的范围是 [1, 9]。
+ * 给定 k 的范围是[1,  n!]。
+ * 示例 1:
+ *
+ * 输入: n = 3, k = 3
+ * 输出: "213"
+ * 示例 2:
+ *
+ * 输入: n = 4, k = 9
+ * 输出: "2314"
+ *
+ * @author ziwen
+ * @since 2020-09-17 17:48
+ */
+public class Solution014 {
+
+    public static String getPermutation(int n, int k) {
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        // 先算出所有的阶乘值
+        for (int i = 1; i < n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        // 这里使用数组或者链表都行
+        List<Integer> nums = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            nums.add(i);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // i 表示剩余的数字个数，初始化为 n - 1
+        for (int i = n - 1; i >= 0; i--) {
+            int index = k / factorial[i] ;
+            if (k % factorial[i] == 0) {
+                index = index - 1;
+            }
+            stringBuilder.append(nums.remove(index));
+            k -= index * factorial[i];
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution014.getPermutation(4, 9));
+    }
+}
